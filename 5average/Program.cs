8bf8db9b26d9c8604872a0c7577b24aa9401cc08
OnurK.Dev <88968938+onurkdev@users.noreply.github.com>
2021-10-12 21:35:35 +0300

    Add files via upload

---
+++ 5average/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _5average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum=0;
+            decimal average=0;
+            Console.WriteLine("Lütfen 5 adet sayı giriniz");
+           
+            for (int i = 1; i < 6; i++ ) {
+                Console.WriteLine("{0}. sayıyı giriniz", i);
+                int input = Int32.Parse(Console.ReadLine());
+                sum += input;
+                }
+            average = Convert.ToDecimal(sum) / 5;
+            Console.WriteLine("Girdiğiniz sayıların ortalaması {0}.",average);
+            Console.WriteLine("Çıkmak için bir tuşa basın");
+            Console.ReadKey();
+            
+        }
+    }
+}
